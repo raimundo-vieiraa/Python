07aca420eed0fe3707a46047c928bf499ecf2c6c
Raimundo Vieira <raimundoruan61@gmail.com>
2025-05-26 20:03:25 -0300

    Criando um App de Delivery

---
+++ app.py
@@ -0,0 +1,40 @@
+import os
+
+def exibir_nome_do_programa():
+    print("""
+███████████████████████████████████████████████████████████████████████████████████████████
+█▄─▄▄▀█▄─▄▄─█▄─▄███▄─▄█▄─█─▄█▄─▄▄─█▄─▄▄▀█▄─█─▄███▄─▄▄─█▄─▀─▄█▄─▄▄─█▄─▄▄▀█▄─▄▄─█─▄▄▄▄█─▄▄▄▄█
+██─██─██─▄█▀██─██▀██─███▄▀▄███─▄█▀██─▄─▄██▄─▄█████─▄█▀██▀─▀███─▄▄▄██─▄─▄██─▄█▀█▄▄▄▄─█▄▄▄▄─█
+▀▄▄▄▄▀▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄▄▀▀▀▄▀▀▀▄▄▄▄▄▀▄▄▀▄▄▀▀▄▄▄▀▀▀▀▄▄▄▄▄▀▄▄█▄▄▀▄▄▄▀▀▀▄▄▀▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀
+      """)
+
+def exibir_opçoes_do_programa():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurante')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
+
+def finalizar_app():
+    os.system('cls')
+    print('Finalizando operação')
+
+def escolher_opcao():
+    opcao_escolhida=int(input('Escolha uma opção: '))
+    if opcao_escolhida==1:
+        print('Restaurantes Cadastrados')
+    elif opcao_escolhida==2:
+        print('Listar Restaurante')
+    elif opcao_escolhida==3:
+        print('Ativar Restaurante')
+    else:
+        finalizar_app
+
+
+
+def main():
+    exibir_nome_do_programa()
+    finalizar_app
+    exibir_opçoes_do_programa()
+    escolher_opcao()
+if __name__ == '__main__':
+    main()
---
+++ att.py
@@ -0,0 +1,10 @@
+print('Python na Escola de Programação Alura\n')
+
+nome='Raimundo'
+idade=18
+print(f'Meu nome é {nome} e tenho {idade} anos.')
+print('A','L','U','R','A',sep='\n')
+pi=3.14159
+print(f'O valor de pi é: {pi:.2f}') # Para determina o numero de casa utilizamos o {var:.2f}
+print('Podemos utilizar tbm uma função chamada round')
+print('O valor de pi é:',round(pi, 2)) # O round arredonda para o numéro inteiro mais próximo ou podemos tbm escolher quandos casas decimais tera: round(var, 2)
