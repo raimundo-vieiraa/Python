97efc1876f84059a871c27b2b78f4d18fd35b70f
Raimundo Vieira <raimundoruan61@gmail.com>
2025-05-27 00:54:53 -0300

    ...

--- app.py
+++ app.py
@@ -8,6 +8,19 @@ def exibir_nome_do_programa():
 ▀▄▄▄▄▀▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄▄▀▀▀▄▀▀▀▄▄▄▄▄▀▄▄▀▄▄▀▀▄▄▄▀▀▀▀▄▄▄▄▄▀▄▄█▄▄▀▄▄▄▀▀▀▄▄▀▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀
       """)
 
+def exibir_opçoes_do_programa():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurante')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
+    if opcao_escolhida==1:
+        print('Restaurantes Cadastrados')
+    elif opcao_escolhida==2:
+        print('Listar Restaurante')
+    elif opcao_escolhida==3:
+        print('Ativar Restaurante')
+    else:
+        finalizar_app
 
 def finalizar_app():
     os.system('cls')
