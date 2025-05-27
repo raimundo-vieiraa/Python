a597c1e98c24b94d5f992eca8e254145bb4c7a1c
Raimundo Vieira <raimundoruan61@gmail.com>
2025-05-27 01:07:38 -0300

    att

--- app.py
+++ app.py
@@ -14,7 +14,9 @@ def exibir_opçoes_do_programa():
     print('3. Ativar restaurante')
     print('4. Sair\n')
 
-
+def finalizar_app():
+    os.system('cls')
+    print('Finalizando operação')
 
 def escolher_opcao():
     opcao_escolhida=int(input('Escolha uma opção: '))
@@ -25,16 +27,11 @@ def escolher_opcao():
     elif opcao_escolhida==3:
         print('Ativar Restaurante')
     else:
-        finalizar_app
-
-def finalizar_app():
-    os.system('cls')
-    print('Finalizando operação')
+        finalizar_app()
 
 def main():
     exibir_nome_do_programa()
     exibir_opçoes_do_programa()
     escolher_opcao()
-    finalizar_app()
 if __name__ == '__main__':
     main()
