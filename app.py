1c5533b07eea09f41d254e1ea3924d2e97dc51ad
Raimundo Vieira <raimundoruan61@gmail.com>
2025-05-27 00:49:09 -0300

    Mudanças na forma de exibição

--- app.py
+++ app.py
@@ -8,33 +8,14 @@ def exibir_nome_do_programa():
 ▀▄▄▄▄▀▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄▄▀▀▀▄▀▀▀▄▄▄▄▄▀▄▄▀▄▄▀▀▄▄▄▀▀▀▀▄▄▄▄▄▀▄▄█▄▄▀▄▄▄▀▀▀▄▄▀▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀
       """)
 
-def exibir_opçoes_do_programa():
-    print('1. Cadastrar restaurante')
-    print('2. Listar restaurante')
-    print('3. Ativar restaurante')
-    print('4. Sair\n')
 
 def finalizar_app():
     os.system('cls')
     print('Finalizando operação')
 
-def escolher_opcao():
-    opcao_escolhida=int(input('Escolha uma opção: '))
-    if opcao_escolhida==1:
-        print('Restaurantes Cadastrados')
-    elif opcao_escolhida==2:
-        print('Listar Restaurante')
-    elif opcao_escolhida==3:
-        print('Ativar Restaurante')
-    else:
-        finalizar_app
-
-
-
 def main():
     exibir_nome_do_programa()
     finalizar_app
-    exibir_opçoes_do_programa()
-    escolher_opcao()
+    finalizar_app()
 if __name__ == '__main__':
     main()
