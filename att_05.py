3ea87e530815425036401c9e2e5adffd16d48e78
Raimundo Vieira <raimundoruan61@gmail.com>
2025-06-05 01:42:29 -0300

    Atividade 05

---
+++ att_05.py
@@ -0,0 +1,30 @@
+"""
+Criar uma classe e colocar 3 exemplos.
+"""
+class Musica:
+    nome=''
+    artista=''
+    duracao= int
+
+musica_salve=Musica()
+musica_salve.nome='Salve Jorge - Ao Vivo'
+musica_salve.artista='Mc Hariel'
+musica_salve.duracao=198
+
+musica_eu_amo_voce=Musica()
+musica_eu_amo_voce='Eu Amo Você'
+musica_eu_amo_voce='Tim Maia'
+musica_eu_amo_voce=243
+
+musica_dias_de_luta=Musica()
+musica_dias_de_luta='Dias de Luta, Dias de Gloria'
+musica_dias_de_luta='Charlie Brown Jr.'
+musica_dias_de_luta=145
+
+
+
+def main():
+    pass
+
+if __name__=='__main__':
+    main()
